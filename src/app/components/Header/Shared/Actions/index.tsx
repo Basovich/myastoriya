66a92cd4555c94c257ca4a6ec8171025ff46cd4f
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-02-20 03:11:44 +0200

    update MainBar

--- src/app/components/Header/Shared/Actions/index.tsx
+++ src/app/components/Header/Shared/Actions/index.tsx
@@ -1,23 +1,34 @@
 import s from "./Actions.module.scss";
 import Image from "next/image";
+import { useAppSelector } from "@/store/hooks";
 
 interface ActionsProps {
     showLogin?: boolean;
 }
 
 export default function Actions({ showLogin = false }: ActionsProps) {
+    const cartItems = useAppSelector((state) => state.cart.items);
+    const wishlistItems = useAppSelector((state) => state.wishlist.items);
+
+    const totalCartItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+    const totalWishlistItems = wishlistItems.length;
+
     return (
         <div className={s.actions}>
             {/* Cart */}
             <button className={s.actionBtn} aria-label="Кошик">
                 <Image src="/icons/shopping-bag.png" alt="Cart" width="20" height="20" />
-                <span className={s.badge}>3</span>
+                {totalCartItems > 0 && (
+                    <span className={s.badge}>{totalCartItems}</span>
+                )}
             </button>
 
             {/* Heart (Favorites) */}
             <button className={s.actionBtn} aria-label="Обране">
                 <Image src="/icons/icon-heart.svg" alt="Favorites" width="20" height="20" />
-                <span className={s.badge}>3</span>
+                {totalWishlistItems > 0 && (
+                    <span className={s.badge}>{totalWishlistItems}</span>
+                )}
             </button>
 
             {/* DESKTOP: Login link */}
