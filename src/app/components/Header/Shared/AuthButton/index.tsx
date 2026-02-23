8723b4d425e343ea9e110427f114ecad5aea620a
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-02-23 23:05:52 +0200

    refactor mobile menu styling

--- src/app/components/Header/Shared/AuthButton/index.tsx
+++ src/app/components/Header/Shared/AuthButton/index.tsx
@@ -22,11 +22,13 @@ export default function AuthButton() {
                 {isAuthenticated ? (
                     <span className={s.avatar}>{firstLetter}</span>
                 ) : (
-                    <Image src="/icons/icon-profile.svg" alt="Profile" width={20} height={20} className={s.icon} />
+                    <>
+                        <span className={s.label}>
+                            {isAuthenticated ? firstLetter : 'Вхід'}
+                        </span>
+                        <Image src="/icons/icon-profile.svg" alt="Profile" width={20} height={20} className={s.icon} />
+                    </>
                 )}
-                <span className={s.label}>
-                    {isAuthenticated ? firstLetter : 'Вхід'}
-                </span>
             </button>
 
             <AuthModal
