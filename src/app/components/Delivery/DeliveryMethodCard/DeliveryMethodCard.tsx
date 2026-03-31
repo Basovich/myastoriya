ba4047e092ec24da644fd007220282da081803df
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-03-31 12:57:16 +0300

    added animation DeliveryMethodCard

--- src/app/components/Delivery/DeliveryMethodCard/DeliveryMethodCard.tsx
+++ src/app/components/Delivery/DeliveryMethodCard/DeliveryMethodCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import s from './DeliveryMethodCard.module.scss';
 import { DeliveryMethodCard as DeliveryMethodCardType } from '@/i18n/types';
+import clsx from "clsx";
 
 interface DeliveryMethodCardProps {
     item: DeliveryMethodCardType;
@@ -55,47 +56,51 @@ const DeliveryMethodCard: React.FC<DeliveryMethodCardProps> = ({ item, hasBackgr
     );
 
     return (
-        <div className={`${s.card} ${hasBackground ? s.hasBackground : ''}`}>
+        <div className={s.cadrWrap}>
             {item.badge && <div className={s.badge}>{item.badge}</div>}
-            <div className={s.imageWrapper}>
-                <Image
-                    src={item.image}
-                    alt={item.title}
-                    fill
-                    className={s.image}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
-                />
-            </div>
-            <div className={s.content}>
-                <h3 className={s.title}>{item.title}</h3>
+            <div className={s.card}>
+                <div className={s.imageWrapper}>
+                    <Image
+                        src={item.image}
+                        alt={item.title}
+                        fill
+                        className={s.image}
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
+                    />
+                </div>
+                <div className={clsx(s.overflowBlock, hasBackground && s.hasBackground)}>
+                    <div className={s.content}>
+                        <h3 className={s.title}>{item.title}</h3>
 
-                <div className={s.cardBody}>
-                    {item.shippingCostLabel && (
-                        <div className={s.infoRow}>
-                            {renderStyledText(item.shippingCostLabel, s.label)}
-                            {item.shippingCostValue && <div className={s.value}>{renderStyledText(item.shippingCostValue)}</div>}
+                        <div className={s.cardBody}>
+                            {item.shippingCostLabel && (
+                                <div className={s.infoRow}>
+                                    {renderStyledText(item.shippingCostLabel, s.label)}
+                                    {item.shippingCostValue && <div className={s.value}>{renderStyledText(item.shippingCostValue)}</div>}
+                                </div>
+                            )}
+
+                            {item.minOrderLabel && (
+                                <div className={s.infoRow}>
+                                    {renderStyledText(item.minOrderLabel, s.label)}
+                                    {item.minOrderValue && <div className={s.value}>{renderStyledText(item.minOrderValue)}</div>}
+                                </div>
+                            )}
+
+                            {isPromo && renderStyledText(firstFeature, s.promoText)}
+
+                            {otherFeatures.length > 0 && (
+                                <ul className={`${s.features} ${item.isPickup ? s.pickupList : ''}`}>
+                                    {otherFeatures.map((feature, index) => (
+                                        <li key={index} className={s.featureItem}>
+                                            {item.isPickup && <div className={s.icon}><LocationIcon /></div>}
+                                            {renderStyledText(feature)}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
                         </div>
-                    )}
-
-                    {item.minOrderLabel && (
-                        <div className={s.infoRow}>
-                            {renderStyledText(item.minOrderLabel, s.label)}
-                            {item.minOrderValue && <div className={s.value}>{renderStyledText(item.minOrderValue)}</div>}
-                        </div>
-                    )}
-
-                    {isPromo && renderStyledText(firstFeature, s.promoText)}
-
-                    {otherFeatures.length > 0 && (
-                        <ul className={`${s.features} ${item.isPickup ? s.pickupList : ''}`}>
-                            {otherFeatures.map((feature, index) => (
-                                <li key={index} className={s.featureItem}>
-                                    {item.isPickup && <div className={s.icon}><LocationIcon /></div>}
-                                    {renderStyledText(feature)}
-                                </li>
-                            ))}
-                        </ul>
-                    )}
+                    </div>
                 </div>
             </div>
         </div>
