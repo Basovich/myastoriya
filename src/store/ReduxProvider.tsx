aebe640d02687f2c18d3cb1a241614f6b2b5fae1
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-04-18 20:04:36 +0300

    added NEXT_PUBLIC_GOOGLE_CLIENT_ID

--- src/store/ReduxProvider.tsx
+++ src/store/ReduxProvider.tsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { store, persistor } from './index';
 
 export default function ReduxProvider({ children }: { children: React.ReactNode }) {
-    const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+    const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '857890084502-u85p6cp357bu5cpntjlcodkcsdm45d9a.apps.googleusercontent.com';
 
     if (!clientId) {
         return (
