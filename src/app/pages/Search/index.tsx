ed28b6d956a978018c91edd41581e2dbf2c14d9d
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-06-05 15:51:47 +0300

    update product card slug and weight handling in related products

--- src/app/pages/Search/index.tsx
+++ src/app/pages/Search/index.tsx
@@ -4,7 +4,7 @@ import SearchContent from "@/app/pages/Search/SearchContent/SearchContent";
 export default function Search() {
     return (
         <>
-            <main style={{ minHeight: "100vh" }}>
+            <main>
                 <SearchContent />
             </main>
         </>
