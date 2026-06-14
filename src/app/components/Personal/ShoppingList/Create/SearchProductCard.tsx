7f7b976f810ef578d130cdb4471f2daad39f25fc
vadim_yakovenko <vadim_yakovenko@struktura.io>
2026-06-14 22:58:13 +0300

    update search card styling, add padding and border-radius, adjust spacing and spacing on hover

--- src/app/components/Personal/ShoppingList/Create/SearchProductCard.tsx
+++ src/app/components/Personal/ShoppingList/Create/SearchProductCard.tsx
@@ -16,16 +16,16 @@ interface SearchProductCardProps {
 export default function SearchProductCard({ name, price, weight, image, onAdd }: SearchProductCardProps) {
     return (
         <div className={s.searchCard}>
-            <div className={s.searchCardImg}>
-                <Image src={image} alt={name} width={120} height={90} />
+            <div className={s.searchCardContent}>
+                <Image src={image} className={s.searchCardImg} alt={name} width={120} height={90} />
+                <div className={s.searchCardInfo}>
+                    <h4 className={s.searchCardName}>{name} <span>{weight && `(${weight})`}</span></h4>
+                    <div className={s.searchCardPrice}>{price.toLocaleString()} <span>₴</span></div>
+                </div>
             </div>
-            <div className={s.searchCardInfo}>
-                <h4 className={s.searchCardName}>{name} {weight && `(${weight})`}</h4>
-                <div className={s.searchCardPrice}>{price.toLocaleString()} ₴</div>
-                <Button variant="outline-black" className={s.addBtn} onClick={onAdd}>
-                    ДОДАТИ ДО СПИСКУ
-                </Button>
-            </div>
+            <Button variant="outline-black" className={s.addBtn} onClick={onAdd}>
+                ДОДАТИ ДО СПИСКУ
+            </Button>
         </div>
     );
 }
