95dae6ad07d24bb5e977e684314e5cb385dd1c83
vadim <vadim.yakovenko86@gmail.com>
2026-07-30 23:26:08 +0300

    refactor shops utils: update Shop import path to use absolute alias for consistency

--- src/lib/utils/shops.ts
+++ src/lib/utils/shops.ts
@@ -1,4 +1,4 @@
-import type { Shop } from "../graphql/queries/shops";
+import type { Shop } from "@/lib/graphql/queries/shops";
 import { Store } from "@/app/components/OurStores/StoreCard/StoreCard";
 
 /**
